abcc6972bbd11db60ec334d98644e8b81138f6f2
Michael Brooks and Senya Webster <michael@>
2017-09-26 13:52:37 -0700

    add passing unit test specs

--- spec/answer_spec.rb
+++ spec/answer_spec.rb
@@ -4,7 +4,7 @@ describe(Answer) do
   describe('#question') do
     it('return the question belonging to the answer') do
       question1 = Question.create({query:"Title of Question"})
-      answer = Answer.create({:input => "Answer this", :question_id => question.id})
+      answer = Answer.create({:input => "Answer this", :question_id => question1.id})
       expect(answer.question).to eq question1
     end
   end
--- spec/question_spec.rb
+++ spec/question_spec.rb
@@ -1,18 +1,19 @@
 require 'spec_helper'
 
 describe(Question) do
-  describe('#answer') do
+  describe('#answers') do
     it('return answers belonging to this question') do
+      survey = Survey.create({:title => "Survey Title"})
       question1 = Question.create({:query => "This is a question1?", :survey_id => survey.id})
-      answer1 = Answer.create({:input => "Answer for question1", :question_id => question.id})
-      answer2 = Answer.create({:input => "Alternate answer for question1", :question_id => question.id})
-      expect(question1.answer).to eq [answer1, answer2]
+      answer1 = Answer.create({:input => "Answer for question1", :question_id => question1.id})
+      answer2 = Answer.create({:input => "Alternate answer for question1", :question_id => question1.id})
+      expect(question1.answers).to eq [answer1, answer2]
     end
   end
   describe('#survey') do
     it('return the survey the questions belongs to') do
       survey1 = Survey.create({:title => "Big Bag Survey"})
-      question = Question.create({:query => "What is a question?", :survey_id => survey.id})
+      question = Question.create({:query => "What is a question?", :survey_id => survey1.id})
       expect(question.survey).to eq survey1
     end
   end
